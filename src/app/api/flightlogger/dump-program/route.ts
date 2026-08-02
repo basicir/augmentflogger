02f94a63479c54bb362b0d8b8fd426754879e81d
basicir <flowcrestapp@gmail.com>
2026-08-02 18:24:46 +0200

    fix: Remove invalid field name from GradedCompetency

--- src/app/api/flightlogger/dump-program/route.ts
+++ src/app/api/flightlogger/dump-program/route.ts
@@ -50,7 +50,6 @@ export async function GET(request: Request) {
                       gradedCompetencies {
                         id
                         coreCompetencyName
-                        name
                       }
                     }
                   }
