f2bad1e2bfe1b901cb1eea0f47ca8da0ee6d6796
basicir <flowcrestapp@gmail.com>
2026-08-03 21:30:57 +0200

    UI Polish: Add text to generic loading screens

--- src/app/student/[id]/loading.tsx
+++ src/app/student/[id]/loading.tsx
@@ -3,6 +3,7 @@ export default function Loading() {
     <div className="page-wrapper">
       <div className="page-loading">
         <span className="spinner" style={{ width: 32, height: 32, borderWidth: 3 }} />
+        <p className="page-loading-text">Loading…</p>
       </div>
     </div>
   );
